f749dcef222d500907b34fd948398eabe09fe86d
Peter Fraenkel <pnf@podsnap.com>
2014-07-16 09:26:07 -0400

    Initial migration

--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
 (defproject awstools "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :author "Peter Fraenkel <http://podsnap.com>"
+  :description "Simple tools for controlling EC2 using core.async"
+  :url "http://github.com/pnf/awstools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.7.0"]
+                 [amazonica "0.2.16"]
+                 [clj-ssh "0.5.10"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [com.taoensso/timbre "3.2.0"] 
+                 [com.taoensso/carmine "2.7.0-RC1"]])
+
+
+
