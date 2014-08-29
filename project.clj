56a569d82013c67ccc6a1801329f6c571254614a
Peter Fraenkel <pnf@podsnap.com>
2014-08-29 17:39:23 -0400

    update async

--- project.clj
+++ project.clj
@@ -5,11 +5,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.codec "0.1.0"]
                  [clj-time "0.7.0"]
                  [amazonica "0.2.16"]
                  [acyclic/utils "0.1.0-SNAPSHOT"]
                  [clj-ssh "0.5.10"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 ;[org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [com.taoensso/timbre "3.2.0"] 
                  [com.taoensso/carmine "2.7.0-RC1"]])
 
